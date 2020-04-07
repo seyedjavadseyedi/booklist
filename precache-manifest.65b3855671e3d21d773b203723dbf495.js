fba34c321bfe9f359a837af97e7eb46003d62891
seyedjavadseyedi <sjsp.seyedi@gmail.com>
2020-04-07 16:58:06 +0430

    Updates

---
+++ precache-manifest.65b3855671e3d21d773b203723dbf495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699c838d4ec7a8ab4dfc1897acba53f6",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "b380f2c5eeff319303ea",
+    "url": "/booklist/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "7744b44c447cdcb9acf6",
+    "url": "/booklist/static/js/2.6b07e8ee.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/booklist/static/js/2.6b07e8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b380f2c5eeff319303ea",
+    "url": "/booklist/static/js/main.f999f504.chunk.js"
+  },
+  {
+    "revision": "6604fd91cd02364f6515",
+    "url": "/booklist/static/js/runtime-main.0e234322.js"
+  }
+]);
